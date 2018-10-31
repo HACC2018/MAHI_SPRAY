4eefc7a8e8c0e4d510284728b721c674e7bfb09d
AppleTesla <jkschrei@gmail.com>
2018-10-30 20:54:51 -1000

    finishing up login page

--- MAHI_SPRAY_APP/ViewController.swift
+++ MAHI_SPRAY_APP/ViewController.swift
@@ -11,16 +11,28 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var loginCard: UIView!
+    @IBOutlet weak var logo_card: UIView!
+    @IBOutlet weak var login_button_card: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loginCard.layer.cornerRadius = 5.0
+        loginCard.layer.cornerRadius = 25.0
+        logo_card_design(ViewToAffect: logo_card)
+        logo_card_design(ViewToAffect: login_button_card)
         
         let layer = CAGradientLayer()
         layer.frame = self.view.bounds
         layer.colors = [UIColor.init(red: 0.301, green: 0.973, blue: 1.0, alpha: 1.0).cgColor, UIColor.init(red: 0.25882, green: 0.7137, blue: 0.9568, alpha: 1.0).cgColor]
         view.layer.insertSublayer(layer, at: 0)
         
+    }
+    
+    func logo_card_design (ViewToAffect: UIView) {
+        ViewToAffect.layer.cornerRadius = 25.0
+        //ViewToAffect.layer.shadowColor = UIColor.black.cgColor
+        ViewToAffect.layer.shadowOpacity = 0.2
+        //ViewToAffect.layer.shadowOffset = CGSize.zero
+        ViewToAffect.layer.shadowRadius = 10
     }
     
     
