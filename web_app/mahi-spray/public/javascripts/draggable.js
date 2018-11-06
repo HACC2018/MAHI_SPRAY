d9f6577e5cc28fef60964a308cffaa25a1d7bfee
Kelemete Seto <{11447446}+{kelemeteseto}@users.noreply.github.com>
2018-11-05 21:20:51 -1000

    added jquery

---
+++ web_app/mahi-spray/public/javascripts/draggable.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#pesticide1').draggable({
+        // axis: 'x',
+        cursor: 'pointer', 
+        opacity: 0.50
+    });
+    $('#pesticide2').draggable({        
+        // axis: 'x',
+        cursor: 'pointer',
+        opacity: 0.50
+    });
+    $('#pesticide3').draggable({
+        // axis: 'x',
+        cursor: 'pointer',
+        opacity: 0.50
+    });
+});
